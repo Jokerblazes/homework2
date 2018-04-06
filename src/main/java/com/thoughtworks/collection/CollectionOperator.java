85cb4736b736d711d76957fae53b0c8808dc7b89
Jokerblazes <zcd123@qq.com>
2018-04-06 15:32:11 +0800

    CollectionOperator类功能实现并测试完成。

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -1,31 +1,75 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.List;
+import java.util.*;
 
 public class CollectionOperator {
+
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        return getIntegers(left, right,false);
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new NotImplementedException();
+        return getIntegers(left, right, true);
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        for (int i : array) {
+            if (isAEven(i))
+                result.add(i);
+        }
+        return result;
     }
 
     public int popLastElment(int[] array) {
-        throw new NotImplementedException();
+        return array[array.length - 1];
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
-        throw new NotImplementedException();
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+        for (int i : firstArray)
+            map.merge(i,1,(integer, integer2) -> integer + integer2);
+        for (int i : secondArray) {
+            Integer integer = map.get(i);
+            if (integer != null) {
+                if (integer == 0)
+                    map.remove(i);
+                else
+                    map.put(i,integer - 1);
+                result.add(i);
+            }
+        }
+        return result;
     }
 
     public List<Integer> addUncommonElement(Integer[] firstArray, Integer[] secondArray) {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        Set<Integer> set = new LinkedHashSet<>();
+        Collections.addAll(set, firstArray);
+        Collections.addAll(set, secondArray);
+        result.addAll(set);
+        return result;
+    }
+
+    private boolean isAEven(int i) {
+        return i % 2 == 0;
+    }
+
+    private List<Integer> getIntegers(int left, int right,boolean needEven) {
+        List<Integer> result = new ArrayList<>();
+        if (left > right) {
+            for (int i = left; i >= right ; i--) {
+                if (!needEven || isAEven(i))
+                    result.add(i);
+            }
+        } else {
+            for (int i = left; i <= right ; i++) {
+                if (!needEven || isAEven(i))
+                    result.add(i);
+            }
+        }
+        return result;
     }
 }
