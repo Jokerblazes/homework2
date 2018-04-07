0049d2ad88a7410ea0bad32b3e0e57f245afb368
Jokerblazes <zcd123@qq.com>
2018-04-07 10:23:18 +0800

    Reduce类功能实现并测试完成。

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -1,8 +1,9 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Reduce {
 
@@ -13,43 +14,89 @@ public class Reduce {
     }
 
     public int getMaximum() {
-        throw new NotImplementedException();
+        return arrayList.stream().max(Comparator.comparingInt(a -> a)).get();
     }
 
     public double getMinimum() {
-        throw new NotImplementedException();
+        return arrayList.stream().min(Comparator.comparingInt(a -> a)).get();
     }
 
     public double getAverage() {
-        throw new NotImplementedException();
+        int sum = 0;
+        for (Integer integer : arrayList) {
+            sum += integer;
+        }
+        return (double) sum / arrayList.size();
     }
 
     public double getOrderedMedian() {
-        throw new NotImplementedException();
+        List<Integer> collect = arrayList.stream().sorted().collect(Collectors.toList());
+        int size = collect.size();
+        int index = size / 2;
+        if (isAEven(size)) {
+            return (double) (collect.get(index - 1) + collect.get(index)) / 2;
+        } else {
+            return (double) collect.get(index);
+        }
     }
 
     public int getFirstEven() {
-        throw new NotImplementedException();
+        return getIndexOfFirstEvenOrFirstEven(false);
     }
 
     public int getIndexOfFirstEven() {
-        throw new NotImplementedException();
+        return getIndexOfFirstEvenOrFirstEven(true);
     }
 
     public boolean isEqual(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        if (arrayList.equals(this.arrayList))
+            return true;
+        return false;
     }
 
     //实现接口SingleLink，然后再此函数内使用
-    public Double getMedianInLinkList(SingleLink singleLink) {
-        throw new NotImplementedException();
-    }
 
+    public Double getMedianInLinkList(SingleLink singleLink) {
+        singleLink.addHeadPointer(arrayList.get(0));
+        for (Integer integer : arrayList) {
+            singleLink.addTailPointer(integer);
+        }
+        int size = arrayList.size();
+        int index = size / 2;
+        if (isAEven(size)) {
+            return (double) ((Integer)singleLink.getNode(index + 1) + (Integer) singleLink.getNode(index)) / 2;
+        } else {
+            return (double) singleLink.getNode(index);
+        }
+    }
     public int getLastOdd() {
-        throw new NotImplementedException();
+        return getIndexOfLastOddOrLastOdd(false);
     }
 
     public int getIndexOfLastOdd() {
-        throw new NotImplementedException();
+        return getIndexOfLastOddOrLastOdd(true);
+    }
+
+    private boolean isAEven(int size) {
+        return size % 2 == 0;
+    }
+
+    private int getIndexOfFirstEvenOrFirstEven(boolean isIndex) {
+        int size = arrayList.size();
+        for (int i = 0; i < size; i++) {
+            if (isAEven(arrayList.get(i))) {
+                return isIndex ? i : arrayList.get(i);
+            }
+        }
+        return -1;
+    }
+
+    private int getIndexOfLastOddOrLastOdd(boolean isIndex) {
+        int size = arrayList.size();
+        for (int i = size - 1; i >= 0; i--) {
+            if (!isAEven(arrayList.get(i)))
+                return isIndex == true ? i : arrayList.get(i);
+        }
+        return -1;
     }
 }
